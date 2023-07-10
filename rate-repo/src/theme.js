aa1bd4f00856ff26177c9c70451b97199eaefbeb
Jonathan Tshikala <jonathanrose606@gmail.com>
2023-07-10 12:10:38 +0200

    platform specific fonts

--- rate-repo/src/theme.js
+++ rate-repo/src/theme.js
@@ -1,32 +1,35 @@
+import { Platform } from "react-native";
+
 const theme = {
   colors: {
     textPrimary: "#24292e",
     textSecondary: "#586069",
     primary: "white",
-    accent:"#0366d6",
-    error: "#d73a4a"
+    accent: "#0366d6",
+    error: "#d73a4a",
   },
   fontSize: {
     body: 16,
     header: 20,
   },
   fonts: {
-    main: "System",
+    default: "System",
+    main: Platform.OS === "android" ? "Roboto" : "Arial",
   },
   fontWeight: {
     normal: "400",
     bold: "700",
   },
-  padding:{
+  padding: {
     normal: 10,
     large: 15,
-    big:20
+    big: 20,
   },
-  borderRadius:{
+  borderRadius: {
     small: 5,
     medium: 10,
-    large: 15
-  }
+    large: 15,
+  },
 };
 
 export default theme;
